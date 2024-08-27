777e141872920565cf46f17f30cebf8797659b23
Yanik <yanikoberheid@gmail.com>
2024-08-27 23:59:13 +0200

    authenticate for controllers

--- Webapp/backend/rest/tracker.js
+++ Webapp/backend/rest/tracker.js
@@ -4,21 +4,21 @@ const trackerController = require('../controller/trackerController');
 const authenticate = require('../middleware/auth');
 
 // Route to get all trackers
-router.get('/',  trackerController.getAllTrackers);
+router.get('/', authenticate, trackerController.getAllTrackers);
 
 // Route to get a single tracker by ID
-router.get('/:id',  trackerController.getTrackerById);
+router.get('/:id',authenticate,  trackerController.getTrackerById);
 
 // Route to create a new tracker
-router.post('/', trackerController.createTracker);
+router.post('/',authenticate, trackerController.createTracker);
 
 // Route to update a tracker by ID
-router.put('/:id',  trackerController.updateTrackerName);
+router.put('/:id', authenticate, trackerController.updateTrackerName);
 
 // Route to delete a tracker by ID
-router.delete('/:id',  trackerController.deleteTracker);
+router.delete('/:id', authenticate, trackerController.deleteTracker);
 
 // Route to set the mode of a tracker by ID
-router.put('/:id/mode',  trackerController.setTrackerMode);
+router.put('/:id/mode', authenticate, trackerController.setTrackerMode);
 
 module.exports = router;
