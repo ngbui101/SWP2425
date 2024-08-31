819aaca871b01c49ae02abfe1adcb76b8a06d54d
Yanik <yanikoberheid@gmail.com>
2024-08-31 19:46:15 +0200

    added vue-i18n for different languages

--- Webapp/frontend/src/main.js
+++ Webapp/frontend/src/main.js
@@ -4,12 +4,28 @@ import App from './App.vue'
 import router from "./routes"
 import {createPinia } from 'pinia'
 import {authentication} from './plugins/authentication'
+import EN from './locale/en.json'
+import DE from './locale/de.json'
+import { createI18n } from 'vue-i18n'
+import './global.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'ionicons/dist/css/ionicons.min.css';
+
+const i18n = createI18n({
+    legacy: false,
+    locale: 'DE',
+    messages: {
+        EN: EN,
+        DE: DE,
+    }
+})
 
 
 const app = createApp(App)
 app.use(createPinia())
 authentication.install().then(() => {
     app.use(router)
+    app.use(i18n)
     app.mount('#app')
     console.log('App mounted successfully:', app);
 })
