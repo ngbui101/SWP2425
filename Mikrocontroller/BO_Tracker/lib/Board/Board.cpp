680c033a76b500b9971cc2df0fb54e946729270a
Lam <buinguyenlam@gmail.com>
2024-12-05 17:18:31 +0100

    add RTC

--- Mikrocontroller/BO_Tracker/lib/Board/Board.cpp
+++ Mikrocontroller/BO_Tracker/lib/Board/Board.cpp
@@ -10,3 +10,23 @@ boolean _Board::initBoard()
     else
         return false;
 }
+
+boolean _Board::setupRTC(uint8_t hh, uint8_t mm, uint8_t ss, uint8_t _dd, uint8_t _mm, uint8_t _yyyy)
+{
+    rtc.setHours(hh);
+    rtc.setMinutes(mm);
+    rtc.setSeconds(ss);
+
+    rtc.setDay(_dd);
+    rtc.setMonth(_mm);
+    rtc.setYear(_yyyy);
+}
+
+bool _Board::getDateTime(JsonDocument &docInput)
+{
+    uint32_t unixTime = rtc.getEpoch();
+    
+    docInput["Timestamp"] = unixTime;
+
+    return true;
+}
--- Mikrocontroller/BO_Tracker/lib/Board/board.h
+++ Mikrocontroller/BO_Tracker/lib/Board/board.h
@@ -1,12 +1,19 @@
 
 #ifndef __BOARD_H_
 #define __BOARD_H_
-
+#include <RTCZero.h>
 #include "Temperature.h"
+#include "ArduinoJson.h"
+
+RTCZero rtc;
 
 class _Board : public _Temperature{
     public:
     _Board();
     boolean initBoard();
+
+    bool setupRTC(uint8_t hh, uint8_t mm, uint8_t ss, uint8_t _dd, uint8_t _mm, uint8_t _yyyy);
+
+    bool getDateTime(JsonDocument &docInput);
 };
 #endif
