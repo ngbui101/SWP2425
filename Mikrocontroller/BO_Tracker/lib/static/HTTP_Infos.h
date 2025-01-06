9871eb8d8e920bdd6698ebb4b864d95ca4627c66
Lam <buinguyenlam@gmail.com>
2025-01-06 16:03:42 +0100

    add HTTP Infos to save static const HTTP Infos

---
+++ Mikrocontroller/BO_Tracker/lib/static/HTTP_Infos.h
@@ -0,0 +1,10 @@
+#ifndef _HTTP_INFOS_H_
+#define _HTTP_INFOS_H_
+
+#include "BG96_HTTP.h"
+
+const HTTP_Body_Data_Type_t http_type = APPLICATION_X_WWW_FORM_URLENCODED;
+
+const char http_url[] PROGMEM = "https://00jqupu0d5.execute-api.eu-central-1.amazonaws.com/post";
+
+#endif // _HTTP_INFOS_H_
