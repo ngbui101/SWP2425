92e5ae18b74d576d2f9f4a407e36dcf2e48a5d69
Lam <buinguyenlam@gmail.com>
2025-01-06 16:06:40 +0100

    add Module: define a superclasse for all modules

---
+++ Mikrocontroller/BO_Tracker/lib/subprograms/Module.h
@@ -0,0 +1,18 @@
+#ifndef _MODULE_H_
+#define _MODULE_H_
+
+#include <Arduino.h>
+#include <Wire.h>
+#include "Logger.h"
+
+class _Module {
+public:
+    _Module();
+    ~_Module();
+protected:
+    Logger initLogger;
+    Logger runLogger;
+private:
+};
+
+#endif // _MODULE_H_
