4af9bc9623462615249ec161771b270b89f076c0
Lam <buinguyenlam@gmail.com>
2025-01-06 23:13:01 +0100

    complete real time mode

--- Mikrocontroller/BO_Tracker/lib/Mode/RealTimeMode.cpp
+++ Mikrocontroller/BO_Tracker/lib/Mode/RealTimeMode.cpp
@@ -5,18 +5,58 @@ RealTimeMode::RealTimeMode(Tracker &trackerObj)
 {
 }
 
-
-void RealTimeMode::start() {
+void RealTimeMode::start()
+{
     setup();
     loop();
 }
 
-
-void RealTimeMode::setup() {
-    
+void RealTimeMode::setup()
+{
+    if (!tracker.isModemAvailable())
+    {
+        tracker.turnOnModem();
+    }
+    if (!tracker.isMQTTAvaliable())
+    {
+        tracker.startMQTT();
+    }
+    if (!tracker.isGnssModuleEnable())
+    {
+        tracker.TurnOnGNSS();
+    }
 }
 
 // Hauptschleife (z. B. zyklische Abfragen, Publikationen etc.)
-void RealTimeMode::loop() {
-    
+void RealTimeMode::loop()
+{
+    unsigned long pub_time = 0;
+    bool keepRunning = true;
+    while (keepRunning)
+    {
+        unsigned long start_time = millis();
+        char response[1028];
+        Mqtt_Event_t event = tracker.waitForResponse(response);
+        switch (event)
+        {
+        case MQTT_RECV_DATA_EVENT:
+            tracker.setMode(response);
+            keepRunning = false;
+            break;
+        case MQTT_CLIENT_CLOSED:
+            keepRunning = false;
+            break;
+        default:
+            break;
+        }
+        if ((start_time - pub_time) >= trackerModes.period)
+        {
+            tracker.modeHandle();
+            if (tracker.publishData("/pub"))
+            {
+                pub_time = millis();
+            }
+        }
+        delay(1000);
+    }
 }
