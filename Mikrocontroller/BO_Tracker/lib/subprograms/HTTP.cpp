896d2a69139beabd5256626264dc28ea2fa0a946
Lam <buinguyenlam@gmail.com>
2025-01-06 16:05:28 +0100

    add HTTP to subprograms

---
+++ Mikrocontroller/BO_Tracker/lib/subprograms/HTTP.cpp
@@ -0,0 +1,44 @@
+#include "HTTP.h"
+
+HTTP::HTTP(Stream &atSerial, Stream &dSerial)
+    : Modem(atSerial, dSerial)
+{
+}
+
+HTTP::~HTTP() {}
+
+bool HTTP::initHTTP()
+{
+    if (!_BG96.SetHTTPConfigParameters(PDPIndex, false, false, http_type))
+    {
+        initLogger.logError("HTTP_Parameters");
+    }
+    if (!_BG96.HTTPURL(http_url, WRITE_MODE))
+    {
+        initLogger.logError("HTTP_Set_URL");
+    }
+    return true;
+}
+
+bool HTTP::sendPostRequest(char *payload)
+{
+    if (!_BG96.HTTPPOST(payload, 80))
+    {
+        return false;
+    }
+    return true;
+}
+
+bool HTTP::readResponse(char *recv_data)
+{
+    if (!_BG96.HTTPRead(recv_data, 80))
+    {
+        return false;
+    }
+    return true;
+}
+
+bool HTTP::sendAndReadResponse(char *payload, char *recv_data){
+    return (sendPostRequest(payload) && readResponse(recv_data));
+}
+
---
+++ Mikrocontroller/BO_Tracker/lib/subprograms/HTTP.h
@@ -0,0 +1,18 @@
+#ifndef _HTTP_H_
+#define _HTTP_H_
+
+#include "Modem.h"
+#include "HTTP_Infos.h"
+
+class HTTP : public Modem {
+public:
+    HTTP(Stream &atSerial, Stream &dSerial);
+    ~HTTP();
+
+    bool initHTTP();
+    bool sendPostRequest(char* payload);
+    bool readResponse(char *recv_data);
+    bool sendAndReadResponse(char* payload,char *recv_data);
+};
+
+#endif // _HTTP_H_
